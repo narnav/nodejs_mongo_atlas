2927dd88c78233c04126462f83c2d0e728f3ad31
eyalg <narnavg@gmail.com>
2022-12-05 12:52:34 +0200

    first commit

---
+++ database.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // url: "mongodb://localhost:27017/"
+    //remote -working good
+    url: 'mongodb+srv://eyal:2014itay@cluster0.t7e23.mongodb.net/test?authSource=admin&replicaSet=atlas-gdrzt5-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dbConfig = require("./database.config");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
+
+// create express app
+const app = express();
+app.use(express.json());
+
+// define a simple route
+app.get("/", (req, res) => {
+  res.json({
+    message:
+      "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes.",
+  });
+});
+
+// listen for requests
+require("./note.routes")(app);
+app.listen(3005, () => {
+  console.log("Server is listening on port 3005");
+});
